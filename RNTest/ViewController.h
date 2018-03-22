2538071ef92cdad6ffb7248fb41e6fb95d326360
changjian01 <768025549@qq.com>
2018-03-22 11:31:32 +0800

    first commit

---
+++ RNTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RNTest
+//
+//  Created by changjian on 2018/3/21.
+//  Copyright © 2018年 changjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RNTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RNTest
+//
+//  Created by changjian on 2018/3/21.
+//  Copyright © 2018年 changjian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
